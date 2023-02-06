c668ea568d4faa7eeae2fac8dc6753b6cf3738d4
root <gloria.kitili@student.moringaschool.com>
2023-02-06 12:58:58 +0300

    exploring ruby with IRB how to enter into IRB,use puts(nil),to exit

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 #app.rb
 #p [1, 2, 3]
 
-pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+#pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+puts "Hello IRB"
